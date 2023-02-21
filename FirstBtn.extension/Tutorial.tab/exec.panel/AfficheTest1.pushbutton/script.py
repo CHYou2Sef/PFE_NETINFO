cef4e61705928bd4aac05dbc2574ddd9fb8b8554
Youssef Chebel <youssef.chebel@gmail.com>
2023-02-21 18:04:35 +0100

    Just a modifying title of script.py -AfficheTest1.pushbutton-

--- FirstBtn.extension/Tutorial.tab/exec.panel/AfficheTest1.pushbutton/script.py
+++ FirstBtn.extension/Tutorial.tab/exec.panel/AfficheTest1.pushbutton/script.py
@@ -1,3 +1,10 @@
+
+__title__ = "Result Material Test"
+__author__ = "Chebl Youssef"
+__doc__ =   """
+This is the button for showing the result of AI code ...
+"""
+
 print("*****")
 f=open("C:\\Users\\YOUSSEF\\test.txt","r")
 print (f.read())
