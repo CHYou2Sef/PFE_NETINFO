e576a0c38f5cac8f60fd0d6ee6270ab568830b22
Youssef Chebel <youssef.Chebel@gmail.com>
2023-03-30 01:10:17 +0100

    i just can modify in the comment , mark but i don't find haw chang material type because i don't haw shared Parameters

--- FirstBtn.extension/Tutorial.tab/Exemple.panel/ChangeWall.pushbutton/script.py
+++ FirstBtn.extension/Tutorial.tab/Exemple.panel/ChangeWall.pushbutton/script.py
@@ -1,3 +1,8 @@
+
+__name__ = "Change element "
+__title__ = "change selected wall"
+__doc__   = "Version 0.1"
+
 import clr
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
@@ -5,8 +10,7 @@ from Autodesk.Revit.DB import *
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-# Récupérer les éléments sélectionnés dans le document
-selection = uidoc.Selection.GetElementIds()
+selection = __revit__.ActiveUIDocument.Selection
 
 # Prompt the user to select a wall
 selected_wall = selection.PickObject(ObjectType.Element, "Select a wall")
@@ -14,18 +18,17 @@ selected_wall = selection.PickObject(ObjectType.Element, "Select a wall")
 # Get the Element ID of the selected wall
 wall_id = selected_wall.ElementId
 
-# Vérifier si un élément a été sélectionné
 if len(selection) != 1:
-    raise Exception('Sélectionnez un mur.')
+    raise Exception("Select a wall.")
 
-# Récupérer l'élément sélectionné
+# Get selected item
 selected_wall = doc.GetElement(selection.First())
 
-# Changer le matériau du mur sélectionné
-t = Transaction(doc, 'Changer le matériau du mur')
+# Change the material of the selected wall
+t = Transaction(doc, __title__)
 t.Start()
 
-material_id = ElementId(wall_id) # Remplacer 123456 par l'ID du matériau souhaité
+material_id = ElementId(wall_id) # Replace 123456 with the desired material ID
 selected_wall.Parameter[BuiltInParameter.WALL_STRUCTURAL_SIGNIFICANT].Set(material_id)
 
 t.Commit()
--- FirstBtn.extension/Tutorial.tab/abc.panel/A.pushbutton/script.py
+++ FirstBtn.extension/Tutorial.tab/abc.panel/A.pushbutton/script.py
@@ -1,5 +1,5 @@
 __name__ = "Select element"
-__title__ = "select wall"
+__title__ = "Get ID"
 __doc__   = "Version 1.0"
 
 import clr
@@ -14,10 +14,10 @@ doc = __revit__.ActiveUIDocument.Document
 selection = __revit__.ActiveUIDocument.Selection
 
 # Prompt the user to select a wall
-selected_wall = selection.PickObject(ObjectType.Element, "Select a wall")
+selected_element = selection.PickObject(ObjectType.Element, "Select an element ")
 
 # Get the Element ID of the selected wall
-wall_id = selected_wall.ElementId
+element_id = selected_element.ElementId
 
 # Print the Element ID to the Revit Python Shell
-print("Selected wall ID: {}".format(wall_id))
+print("Selected Element ID: {}".format(element_id))
